685cd036e626e616b8b0d65e0ba79d430ca304ed
Emiliano <atalaemiliano2@gmail.com>
2019-11-28 12:14:50 -0300

    IYouThey implement websocket

--- iy-morty/src/App.js
+++ iy-morty/src/App.js
@@ -5,21 +5,49 @@ import ApolloClient, { InMemoryCache } from "apollo-boost";
 import { config } from './config';
 import { ROUTES } from './helpers/routes';
 import DashboardContainer from './components/dashboard/DashboardContainer';
+import { split } from 'apollo-link';
+import { HttpLink } from 'apollo-link-http';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
 
-const client = new ApolloClient({
-  uri: config.SERVER_URI,
-  cache: new InMemoryCache({
-    addTypename: false
-  }),
-  onError: ({ networkError, graphQLErrors }) => {
-    console.log("graphQLErrors", graphQLErrors);
-    console.log("networkError", networkError);
+const httpLink = new HttpLink({
+  uri: config.SERVER_URI
+});
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:5060/`,
+  options: {
+    reconnect: true
   }
 });
 
+const link = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink,
+);
+
+
+// const client = new ApolloClient({
+//   uri: config.SERVER_URI,
+//   cache: new InMemoryCache({
+//     addTypename: false
+//   }),
+//   onError: ({ networkError, graphQLErrors }) => {
+//     console.log("graphQLErrors", graphQLErrors);
+//     console.log("networkError", networkError);
+//   }
+// });
+
 function App() {
   return (
-    <ApolloProvider client={client}>
+    <ApolloProvider client={link}>
       <Router>
             <Switch>
               <Route path={ROUTES.Dashboard} component={DashboardContainer}/>
