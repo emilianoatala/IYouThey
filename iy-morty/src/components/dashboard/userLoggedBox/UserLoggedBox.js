b0403afd08ec3aa2ec9985a71c39f94dc3564c1e
Emiliano <atalaemiliano2@gmail.com>
2019-12-06 17:44:18 -0300

    IYouThey add user into room

--- iy-morty/src/components/dashboard/userLoggedBox/UserLoggedBox.js
+++ iy-morty/src/components/dashboard/userLoggedBox/UserLoggedBox.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import './UserLoggedBox.scss'
 import UserCard from '../userCard/UserCard';
+import { USER_LOGGED } from '../../../subscription';
 
-const UserLoggedBox = () => {
+const UserLoggedBox = ({array}) => {
+    const [users, setUsers]= useState([])
+    const logged = useSubscription(USER_LOGGED)
+    const disconnected = useSubscription(USER_DISCONNECTED)
+    const {data, loading, error} = useQuery(GET_USER_LOGGED)
+    
+
+    useEffect(()=>{
+        setUsers(array)
+    },[] )
+    
+
+    useEffect(()=>{
+        let newUsers={...users}
+        newUsers.push()
+        setUsers(newUsers)
+    },[logged] )
+
+    useEffect(()=>{
+
+    },[disconnected] )
+
     return ( 
         <div className="user-logged-box-container column-container">
             <h3>Users in the Room</h3>
